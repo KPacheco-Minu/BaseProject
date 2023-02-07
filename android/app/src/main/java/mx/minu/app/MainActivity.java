44ff7541eaddb5cf3c52861dc40aa14f61ebbbaf
kev_pacheco <kevin@minu.mx>
2023-02-07 11:22:08 -0600

    Updated android config

--- android/app/src/main/java/mx/minu/app/MainActivity.java
+++ android/app/src/main/java/mx/minu/app/MainActivity.java
@@ -2,4 +2,14 @@ package mx.minu.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+          //add(MyPlugin.class);
+        }});
+    }
+}
